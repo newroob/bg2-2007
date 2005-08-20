d78a63b530e9025a93d832f7028d3cb78c8dfb8e
tjoppen <tjoppen@d519a5cf-78fe-0310-83d0-8aec7336fec8>
2005-08-20 14:42:54 +0000

    muskets shoot balistic bullets

--- dlls/hl2_dll/weapon_bg2base.cpp
+++ dlls/hl2_dll/weapon_bg2base.cpp
@@ -35,8 +35,515 @@
 #include "ammodef.h"
 
 //BG2 - Draco - no hl2 player stuff on server!
-#ifndef CLIENT_DLL
+#ifdef CLIENT_DLL
+	#include "c_hl2mp_player.h"
+	#include "c_te_effect_dispatch.h"
+
+	#include "cbase.h"
+	#include "model_types.h"
+	#include "ClientEffectPrecacheSystem.h"
+	#include "fx.h"
+#else
 	#include "hl2mp_player.h"
+	#include "te_effect_dispatch.h"
+	#include "IEffects.h"
+	#include "Sprite.h"
+	#include "SpriteTrail.h"
+	#include "beam_shared.h"
+
+	#include "shot_manipulator.h"
+#endif
+
+#include "weapon_hl2mpbasehlmpcombatweapon.h"
+#include "effect_dispatch_data.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+
+//#define BOLT_MODEL			"models/crossbow_bolt.mdl"
+#define BOLT_MODEL			"models/game/musket_ball.mdl"
+//#define BOLT_MODEL	"models/weapons/w_missile_closed.mdl"
+
+//#define BOLT_AIR_VELOCITY	3500
+#define BOLT_AIR_VELOCITY	10000
+#define BOLT_WATER_VELOCITY	1500
+#define	BOLT_SKIN_NORMAL	0
+#define BOLT_SKIN_GLOW		1
+
+
+#ifndef CLIENT_DLL
+
+//-----------------------------------------------------------------------------
+// Crossbow Bolt
+//-----------------------------------------------------------------------------
+class CBullet : public CBaseCombatCharacter
+{
+	DECLARE_CLASS( CBullet, CBaseCombatCharacter );
+
+public:
+	CBullet() { };
+	~CBullet();
+
+	Class_T Classify( void ) { return CLASS_NONE; }
+
+public:
+	void Spawn( void );
+	void Precache( void );
+	void BubbleThink( void );
+	void BoltTouch( CBaseEntity *pOther );
+	bool CreateVPhysics( void );
+	unsigned int PhysicsSolidMaskForEntity() const;
+	static CBullet *BoltCreate( const Vector &vecOrigin, const QAngle &angAngles, int iDamage, CBasePlayer *pentOwner = NULL );
+
+protected:
+
+	bool	CreateSprites( void );
+
+	CHandle<CSprite>		m_pGlowSprite;
+	//CHandle<CSpriteTrail>	m_pGlowTrail;
+	
+	int		m_iDamage;
+
+	DECLARE_DATADESC();
+	DECLARE_SERVERCLASS();
+};
+LINK_ENTITY_TO_CLASS( bullet, CBullet );
+
+BEGIN_DATADESC( CBullet )
+	// Function Pointers
+	DEFINE_FUNCTION( BubbleThink ),
+	DEFINE_FUNCTION( BoltTouch ),
+
+	// These are recreated on reload, they don't need storage
+	DEFINE_FIELD( m_pGlowSprite, FIELD_EHANDLE ),
+	//DEFINE_FIELD( m_pGlowTrail, FIELD_EHANDLE ),
+
+END_DATADESC()
+
+IMPLEMENT_SERVERCLASS_ST( CBullet, DT_Bullet )
+END_SEND_TABLE()
+
+CBullet *CBullet::BoltCreate( const Vector &vecOrigin, const QAngle &angAngles, int iDamage, CBasePlayer *pentOwner )
+{
+	// Create a new entity with CBullet private data
+	CBullet *pBolt = (CBullet *)CreateEntityByName( "bullet" );
+	UTIL_SetOrigin( pBolt, vecOrigin );
+	pBolt->SetAbsAngles( angAngles );
+	pBolt->Spawn();
+	pBolt->SetOwnerEntity( pentOwner );
+
+	pBolt->m_iDamage = iDamage;
+
+	return pBolt;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+CBullet::~CBullet( void )
+{
+	if ( m_pGlowSprite )
+	{
+		UTIL_Remove( m_pGlowSprite );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Returns true on success, false on failure.
+//-----------------------------------------------------------------------------
+bool CBullet::CreateVPhysics( void )
+{
+	// Create the object in the physics system
+	VPhysicsInitNormal( SOLID_BBOX, FSOLID_NOT_STANDABLE, false );
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+unsigned int CBullet::PhysicsSolidMaskForEntity() const
+{
+	return ( BaseClass::PhysicsSolidMaskForEntity() | CONTENTS_HITBOX ) & ~CONTENTS_GRATE;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Returns true on success, false on failure.
+//-----------------------------------------------------------------------------
+bool CBullet::CreateSprites( void )
+{
+	// Start up the eye glow
+	m_pGlowSprite = CSprite::SpriteCreate( "sprites/light_glow02_noz.vmt", GetLocalOrigin(), false );
+
+	if ( m_pGlowSprite != NULL )
+	{
+		m_pGlowSprite->FollowEntity( this );
+		m_pGlowSprite->SetTransparency( kRenderGlow, 255, 255, 255, 128, kRenderFxNoDissipation );
+		m_pGlowSprite->SetScale( 0.2f );
+		m_pGlowSprite->TurnOff();
+	}
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CBullet::Spawn( void )
+{
+	Precache( );
+
+	//SetModel( "models/crossbow_bolt.mdl" );
+	SetModel( BOLT_MODEL );
+	SetMoveType( MOVETYPE_FLYGRAVITY, MOVECOLLIDE_FLY_CUSTOM );
+	UTIL_SetSize( this, -Vector(1,1,1), Vector(1,1,1) );
+	SetSolid( SOLID_BBOX );
+	//SetGravity( 0.05f );
+	SetGravity( 1.0f );
+	SetFriction( 100.0f );
+	
+	// Make sure we're updated if we're underwater
+	UpdateWaterState();
+
+	SetTouch( &CBullet::BoltTouch );
+
+	SetThink( &CBullet::BubbleThink );
+	SetNextThink( gpGlobals->curtime + 0.1f );
+	
+	CreateSprites();
+
+	// Make us glow until we've hit the wall
+	m_nSkin = BOLT_SKIN_GLOW;
+}
+
+
+void CBullet::Precache( void )
+{
+	PrecacheModel( BOLT_MODEL );
+
+	// This is used by C_TEStickyBolt, despte being different from above!!!
+	//PrecacheModel( "models/crossbow_bolt.mdl" );
+
+	PrecacheModel( "sprites/light_glow02_noz.vmt" );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : *pOther - 
+//-----------------------------------------------------------------------------
+void CBullet::BoltTouch( CBaseEntity *pOther )
+{
+	if ( !pOther->IsSolid() || pOther->IsSolidFlagSet(FSOLID_VOLUME_CONTENTS) )
+		return;
+
+	if ( pOther->m_takedamage != DAMAGE_NO )
+	{
+		trace_t	tr, tr2;
+		tr = BaseClass::GetTouchTrace();
+		Vector	vecNormalizedVel = GetAbsVelocity();
+
+		ClearMultiDamage();
+		VectorNormalize( vecNormalizedVel );
+
+		if( GetOwnerEntity() && GetOwnerEntity()->IsPlayer() && pOther->IsNPC() )
+		{
+			CTakeDamageInfo	dmgInfo( this, GetOwnerEntity(), m_iDamage, DMG_BULLET | DMG_NEVERGIB );
+			dmgInfo.AdjustPlayerDamageInflictedForSkillLevel();
+			CalculateMeleeDamageForce( &dmgInfo, vecNormalizedVel, tr.endpos, 0.07f );
+			dmgInfo.SetDamagePosition( tr.endpos );
+			pOther->DispatchTraceAttack( dmgInfo, vecNormalizedVel, &tr );
+		}
+		else
+		{
+			CTakeDamageInfo	dmgInfo( this, GetOwnerEntity(), m_iDamage, DMG_BULLET | DMG_NEVERGIB );
+			CalculateMeleeDamageForce( &dmgInfo, vecNormalizedVel, tr.endpos, 0.7f );
+			dmgInfo.SetDamagePosition( tr.endpos );
+			pOther->DispatchTraceAttack( dmgInfo, vecNormalizedVel, &tr );
+		}
+
+		ApplyMultiDamage();
+
+		//Adrian: keep going through the glass.
+		if ( pOther->GetCollisionGroup() == COLLISION_GROUP_BREAKABLE_GLASS )
+			 return;
+
+		SetAbsVelocity( Vector( 0, 0, 0 ) );
+
+		// play body "thwack" sound
+		EmitSound( "Weapon_Crossbow.BoltHitBody" );
+
+		Vector vForward;
+
+		AngleVectors( GetAbsAngles(), &vForward );
+		VectorNormalize ( vForward );
+
+		UTIL_TraceLine( GetAbsOrigin(),	GetAbsOrigin() + vForward * 128, MASK_OPAQUE, pOther, COLLISION_GROUP_NONE, &tr2 );
+
+		if ( tr2.fraction != 1.0f )
+		{
+//			NDebugOverlay::Box( tr2.endpos, Vector( -16, -16, -16 ), Vector( 16, 16, 16 ), 0, 255, 0, 0, 10 );
+//			NDebugOverlay::Box( GetAbsOrigin(), Vector( -16, -16, -16 ), Vector( 16, 16, 16 ), 0, 0, 255, 0, 10 );
+
+			if ( tr2.m_pEnt == NULL || ( tr2.m_pEnt && tr2.m_pEnt->GetMoveType() == MOVETYPE_NONE ) )
+			{
+				CEffectData	data;
+
+				data.m_vOrigin = tr2.endpos;
+				data.m_vNormal = vForward;
+				data.m_nEntIndex = tr2.fraction != 1.0f;
+			
+				//DispatchEffect( "BoltImpact", data );
+				//DispatchEffect( "BulletImpact", data );
+			}
+		}
+		
+		SetTouch( NULL );
+		SetThink( NULL );
+
+		UTIL_Remove( this );
+	}
+	else
+	{
+		trace_t	tr;
+		tr = BaseClass::GetTouchTrace();
+
+		// See if we struck the world
+		if ( pOther->GetMoveType() == MOVETYPE_NONE && !( tr.surface.flags & SURF_SKY ) )
+		{
+			EmitSound( "Weapon_Crossbow.BoltHitWorld" );
+
+			// if what we hit is static architecture, can stay around for a while.
+			Vector vecDir = GetAbsVelocity();
+			float speed = VectorNormalize( vecDir );
+
+			// See if we should reflect off this surface
+			float hitDot = DotProduct( tr.plane.normal, -vecDir );
+			
+			if ( ( hitDot < 0.5f ) && ( speed > 100 ) )
+			{
+				Vector vReflection = 2.0f * tr.plane.normal * hitDot + vecDir;
+				
+				QAngle reflectAngles;
+
+				VectorAngles( vReflection, reflectAngles );
+
+				SetLocalAngles( reflectAngles );
+
+				SetAbsVelocity( vReflection * speed * 0.75f );
+
+				// Start to sink faster
+				SetGravity( 1.0f );
+			}
+			else
+			{
+				SetThink( &CBullet::SUB_Remove );
+				SetNextThink( gpGlobals->curtime + 2.0f );
+				
+				//FIXME: We actually want to stick (with hierarchy) to what we've hit
+				SetMoveType( MOVETYPE_NONE );
+			
+				Vector vForward;
+
+				AngleVectors( GetAbsAngles(), &vForward );
+				VectorNormalize ( vForward );
+
+				CEffectData	data;
+
+				data.m_vOrigin = tr.endpos;
+				data.m_vNormal = vForward;
+				data.m_nEntIndex = 0;
+			
+				//DispatchEffect( "BoltImpact", data );
+				//DispatchEffect( "BulletImpact", data );
+				
+				UTIL_ImpactTrace( &tr, DMG_BULLET );
+
+				AddEffects( EF_NODRAW );
+				SetTouch( NULL );
+				SetThink( &CBullet::SUB_Remove );
+				SetNextThink( gpGlobals->curtime + 2.0f );
+
+				if ( m_pGlowSprite != NULL )
+				{
+					m_pGlowSprite->TurnOn();
+					m_pGlowSprite->FadeAndDie( 3.0f );
+				}
+			}
+			
+			// Shoot some sparks
+			if ( UTIL_PointContents( GetAbsOrigin() ) != CONTENTS_WATER)
+			{
+				g_pEffects->Sparks( GetAbsOrigin() );
+			}
+		}
+		else
+		{
+			// Put a mark unless we've hit the sky
+			if ( ( tr.surface.flags & SURF_SKY ) == false )
+			{
+				UTIL_ImpactTrace( &tr, DMG_BULLET );
+			}
+
+			UTIL_Remove( this );
+		}
+	}
+
+	if ( g_pGameRules->IsMultiplayer() )
+	{
+//		SetThink( &CBullet::ExplodeThink );
+//		SetNextThink( gpGlobals->curtime + 0.1f );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CBullet::BubbleThink( void )
+{
+	QAngle angNewAngles;
+
+	VectorAngles( GetAbsVelocity(), angNewAngles );
+	SetAbsAngles( angNewAngles );
+
+	SetNextThink( gpGlobals->curtime + 0.1f );
+
+	if ( GetWaterLevel()  == 0 )
+		return;
+
+	UTIL_BubbleTrail( GetAbsOrigin() - GetAbsVelocity() * 0.1f, GetAbsOrigin(), 5 );
+}
+
+#else
+
+/*CLIENTEFFECT_REGISTER_BEGIN( PrecacheEffectCrossbow )
+CLIENTEFFECT_MATERIAL( "effects/muzzleflash1" )
+CLIENTEFFECT_REGISTER_END()*/
+
+extern void DrawHalo( IMaterial* pMaterial, const Vector &source, float scale, float const *color );
+
+//
+// Crossbow bolt
+//
+
+class C_Bullet : public C_BaseCombatCharacter
+{
+	DECLARE_CLASS( C_Bullet, C_BaseCombatCharacter );
+	DECLARE_CLIENTCLASS();
+public:
+	
+	C_Bullet( void );
+
+	virtual RenderGroup_t GetRenderGroup( void )
+	{
+		// We want to draw translucent bits as well as our main model
+		return RENDER_GROUP_TWOPASS;
+	}
+
+	virtual void	ClientThink( void );
+
+	virtual void	OnDataChanged( DataUpdateType_t updateType );
+	virtual int		DrawModel( int flags );
+
+private:
+
+	C_Bullet( const C_Bullet & ); // not defined, not accessible
+
+	Vector	m_vecLastOrigin;
+	bool	m_bUpdated;
+};
+
+IMPLEMENT_CLIENTCLASS_DT( C_Bullet, DT_Bullet, CBullet )
+END_RECV_TABLE()
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+C_Bullet::C_Bullet( void )
+{
+	//SetModel( BOLT_MODEL );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : updateType - 
+//-----------------------------------------------------------------------------
+void C_Bullet::OnDataChanged( DataUpdateType_t updateType )
+{
+	BaseClass::OnDataChanged( updateType );
+
+	if ( updateType == DATA_UPDATE_CREATED )
+	{
+		m_bUpdated = false;
+		m_vecLastOrigin = GetAbsOrigin();
+		SetNextClientThink( CLIENT_THINK_ALWAYS );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : flags - 
+// Output : int
+//-----------------------------------------------------------------------------
+int C_Bullet::DrawModel( int flags )
+{
+	// See if we're drawing the motion blur
+	if ( flags & STUDIO_TRANSPARENCY )
+	{
+		float		color[3];
+		IMaterial	*pBlurMaterial = materials->FindMaterial( "effects/muzzleflash1", NULL, false );
+
+		Vector	vecDir = GetAbsOrigin() - m_vecLastOrigin;
+		float	speed = VectorNormalize( vecDir );
+		
+		speed = clamp( speed, 0, 32 );
+		
+		if ( speed > 0 )
+		{
+			float	stepSize = min( ( speed * 0.5f ), 4.0f );
+
+			Vector	spawnPos = GetAbsOrigin() + ( vecDir * 24.0f );
+			Vector	spawnStep = -vecDir * stepSize;
+
+			materials->Bind( pBlurMaterial );
+
+			float	alpha;
+
+			// Draw the motion blurred trail
+			for ( int i = 0; i < 20; i++ )
+			{
+				spawnPos += spawnStep;
+
+				alpha = RemapValClamped( i, 5, 11, 0.25f, 0.05f );
+
+				color[0] = color[1] = color[2] = alpha;
+
+				DrawHalo( pBlurMaterial, spawnPos, 3.0f, color );
+			}
+		}
+
+		if ( gpGlobals->frametime > 0.0f && !m_bUpdated)
+		{
+			m_bUpdated = true;
+			m_vecLastOrigin = GetAbsOrigin();
+		}
+
+		return 1;
+	}
+
+	// Draw the normal portion
+	return BaseClass::DrawModel( flags );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void C_Bullet::ClientThink( void )
+{
+	m_bUpdated = false;
+}
+
 #endif
 
 //-----------------------------------------------------------------------------
@@ -116,7 +623,7 @@ static ConVar sv_turboshots( "sv_turboshots", "0", FCVAR_CHEAT | FCVAR_NOTIFY | 
 static ConVar sv_perfectaim( "sv_perfectaim", "0", FCVAR_CHEAT | FCVAR_NOTIFY | FCVAR_REPLICATED, "No spread for bullet weapons\n" );
 static ConVar sv_steadyhand( "sv_steadyhand", "0", FCVAR_CHEAT | FCVAR_NOTIFY | FCVAR_REPLICATED, "No recoil for bullet weapons\n" );
 
-int CBaseBG2Weapon::Fire( int iAttack )
+/*int CBaseBG2Weapon::Fire( int iAttack )
 {
 	// Only the player fires this way so we can cast
 	CBasePlayer *pPlayer = ToBasePlayer( GetOwner() );
@@ -158,7 +665,7 @@ int CBaseBG2Weapon::Fire( int iAttack )
 		pPlayer->SetAnimation( PLAYER_ATTACK1 );
 	}
 
-	SendWeaponAnim( GetActivity( iAttack ) );*/
+	SendWeaponAnim( GetActivity( iAttack ) );*//*
 
 	WeaponSound(SINGLE);
 
@@ -217,6 +724,98 @@ int CBaseBG2Weapon::Fire( int iAttack )
 		// HEV suit - indicate out of ammo condition
 		pPlayer->SetSuitUpdate( "!HEV_AMO0", FALSE, 0 ); 
 	}
+
+	return 15;
+}*/
+
+int CBaseBG2Weapon::Fire( int iAttack )
+{
+	if ( m_iClip1 <= 0 )
+	{
+		if ( !m_bFireOnEmpty )
+		{
+			Reload();
+		}
+		else
+		{
+			WeaponSound( EMPTY );
+			m_flNextPrimaryAttack = 0.15;
+		}
+
+		return 0;
+	}
+
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+	
+	if ( pOwner == NULL )
+		return 0;
+
+#ifndef CLIENT_DLL
+	Vector vecAiming	= pOwner->GetAutoaimVector( 0 );	
+	Vector vecSrc		= pOwner->Weapon_ShootPosition();
+
+	/*QAngle angAiming;
+	VectorAngles( vecAiming, angAiming );*/
+
+    CShotManipulator Manipulator( vecAiming );
+	Vector vecDir		= Manipulator.ApplySpread( GetSpread( iAttack ) );
+
+	QAngle angDir;
+	VectorAngles( vecDir, angDir );
+
+	CBullet *pBolt = CBullet::BoltCreate( vecSrc, angDir, GetDamage(iAttack), pOwner );
+
+	if ( pOwner->GetWaterLevel() == 3 )
+	{
+		//pBolt->SetAbsVelocity( vecAiming * BOLT_WATER_VELOCITY );
+		pBolt->SetAbsVelocity( vecDir * BOLT_WATER_VELOCITY );
+	}
+	else
+	{
+		//pBolt->SetAbsVelocity( vecAiming * BOLT_AIR_VELOCITY );
+		pBolt->SetAbsVelocity( vecDir * BOLT_AIR_VELOCITY );
+	}
+
+#endif
+
+	WeaponSound( SINGLE );
+	//WeaponSound( SPECIAL2 );
+
+	if( sv_turboshots.GetInt() == 0 )
+	{
+		//pOwner->DoMuzzleFlash();
+
+		//BG2 - Tjoppen -	the weapon animation !!MUST!! play before the player animation or there's
+		//					 no muzzle flash. don't forget this! ever! it's annoying as hell!
+		//SendWeaponAnim( GetPrimaryAttackActivity() );
+
+		// player "shoot" animation
+		//pOwner->SetAnimation( PLAYER_ATTACK1 );
+	}
+	
+	if( sv_turboshots.GetInt() == 0 )
+		m_flNextPrimaryAttack = m_flNextSecondaryAttack = gpGlobals->curtime + GetAttackRate( iAttack );
+	else
+		m_flNextPrimaryAttack = m_flNextSecondaryAttack = gpGlobals->curtime + 0.1f;
+
+	if( sv_infiniteammo.GetInt() == 0 )
+		m_iClip1--;
+
+	if( sv_steadyhand.GetInt() == 0 )
+		pOwner->ViewPunch( QAngle( -2, 0, 0 ) );
+
+	SendWeaponAnim( GetActivity( iAttack ) );
+
+	if ( !m_iClip1 && pOwner->GetAmmoCount( m_iPrimaryAmmoType ) <= 0 )
+	{
+		// HEV suit - indicate out of ammo condition
+		pOwner->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+	}
+
+	/*m_flNextPrimaryAttack = m_flNextSecondaryAttack	= gpGlobals->curtime + 0.75;
+
+	DoLoadEffect();
+	SetChargerState( CHARGER_STATE_DISCHARGE );*/
 
 	return 15;
 }
--- game_shared/movevars_shared.cpp
+++ game_shared/movevars_shared.cpp
@@ -33,7 +33,10 @@ ConVar	sv_rollspeed	( "sv_rollspeed", "200", FCVAR_NOTIFY | FCVAR_REPLICATED);
 ConVar	sv_rollangle	( "sv_rollangle", "0", FCVAR_NOTIFY | FCVAR_REPLICATED, "Max view roll angle");
 ConVar	sv_friction		( "sv_friction","4", FCVAR_NOTIFY | FCVAR_REPLICATED, "World friction." );
 ConVar	sv_bounce		( "sv_bounce","0", FCVAR_NOTIFY | FCVAR_REPLICATED, "Bounce multiplier for when physically simulated objects collide with other objects." );
-ConVar	sv_maxvelocity	( "sv_maxvelocity","3500", FCVAR_REPLICATED, "Maximum speed any ballistically moving object is allowed to attain per axis." );
+//BG2 - Tjoppen - other default value for sv_maxvelocity
+//ConVar	sv_maxvelocity	( "sv_maxvelocity","3500", FCVAR_REPLICATED, "Maximum speed any ballistically moving object is allowed to attain per axis." );
+ConVar	sv_maxvelocity	( "sv_maxvelocity","10000", FCVAR_REPLICATED, "Maximum speed any ballistically moving object is allowed to attain per axis." );
+//
 ConVar	sv_stepsize		( "sv_stepsize","18", FCVAR_NOTIFY | FCVAR_REPLICATED );
 ConVar	sv_skyname		( "sv_skyname", "sky_urb01", FCVAR_ARCHIVE | FCVAR_REPLICATED, "Current name of the skybox texture" );
 ConVar	sv_backspeed	( "sv_backspeed", "0.6", FCVAR_ARCHIVE | FCVAR_REPLICATED, "How much to slow down backwards motion" );
