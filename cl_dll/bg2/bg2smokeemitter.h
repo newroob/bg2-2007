29ea8da39a91c51dafb5b20b58bb6907dd03eaac
tjoppen <tjoppen@d519a5cf-78fe-0310-83d0-8aec7336fec8>
2006-06-13 13:12:59 +0000

    applied Skillet's patch to make smoke avoid walls..

--- cl_dll/bg2/bg2smokeemitter.h
+++ cl_dll/bg2/bg2smokeemitter.h
@@ -4,6 +4,7 @@ class CBG2SmokeEmitter : public CSimpleEmitter
 public:
 	CBG2SmokeEmitter( const char *pDebugName );
 	static CSmartPtr<CBG2SmokeEmitter>	Create( const char *pDebugName );
+	virtual void SimulateParticles( CParticleSimulateIterator *pIterator ); //BG2 - Skillet
 	virtual void UpdateVelocity( SimpleParticle *pParticle, float timeDelta );
 	virtual	float UpdateRoll( SimpleParticle *pParticle, float timeDelta );
 
@@ -11,7 +12,6 @@ private:
 	CBG2SmokeEmitter( const CBG2SmokeEmitter & );
 	Vector	m_vDrift;
 };
-
 
 CBG2SmokeEmitter::CBG2SmokeEmitter( const char *pDebugName ) : CSimpleEmitter( pDebugName )
 {
@@ -30,9 +30,44 @@ CSmartPtr<CBG2SmokeEmitter> CBG2SmokeEmitter::Create( const char *pDebugName )
 	return new CBG2SmokeEmitter( pDebugName );
 }
 
+void CBG2SmokeEmitter::SimulateParticles( CParticleSimulateIterator *pIterator )
+{
+	float timeDelta = pIterator->GetTimeDelta();
 
+	SimpleParticle *pParticle = (SimpleParticle*)pIterator->GetFirst();
+	while ( pParticle )
+	{
+		//Update velocity
+		UpdateVelocity( pParticle, timeDelta );
+		pParticle->m_Pos += pParticle->m_vecVelocity * timeDelta;
+
+		//Should this particle die?
+		pParticle->m_flLifetime += timeDelta;
+		UpdateRoll( pParticle, timeDelta );
+
+		if ( pParticle->m_flLifetime >= pParticle->m_flDieTime )
+			pIterator->RemoveParticle( pParticle );
+
+		pParticle = (SimpleParticle*)pIterator->GetNext();
+	}
+}
+
+#define REFLECTION_DAMPENING 0.1f
 void CBG2SmokeEmitter::UpdateVelocity( SimpleParticle *pParticle, float timeDelta )
 {
+	//BG2 - Skillet - Collision Detection
+	Vector vecDir = pParticle->m_vecVelocity;
+	VectorNormalize( vecDir );
+
+	trace_t tr;
+	UTIL_TraceLine( pParticle->m_Pos, pParticle->m_Pos + ( vecDir * (3 + CSimpleEmitter::UpdateScale( pParticle )) ), MASK_SOLID_BRUSHONLY, NULL, &tr );
+	if ( tr.DidHit() && !(tr.surface.flags & SURF_SKY) )
+	{
+		float proj = pParticle->m_vecVelocity.Dot( tr.plane.normal );
+		pParticle->m_vecVelocity += tr.plane.normal * (-proj*2.0f); //Reflection
+		pParticle->m_vecVelocity *= random->RandomFloat( (REFLECTION_DAMPENING-0.1f), (REFLECTION_DAMPENING+0.1f) ); //Dampening
+	}
+
 	//slow down...
 	pParticle->m_vecVelocity *= expf( DROPOFF * timeDelta );
 
@@ -46,4 +81,3 @@ float CBG2SmokeEmitter::UpdateRoll( SimpleParticle *pParticle, float timeDelta )
 
 	return pParticle->m_flRoll;
 }
-//
