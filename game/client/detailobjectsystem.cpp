7323fc1d69a833edf3ce23c85e4652daabd29024
bgmod <bgmod@d519a5cf-78fe-0310-83d0-8aec7336fec8>
2010-02-15 21:07:42 +0000

    New default detail values.

--- game/client/detailobjectsystem.cpp
+++ game/client/detailobjectsystem.cpp
@@ -49,9 +49,9 @@ ConVar cl_detaildist( "cl_detaildist", "1200", 0, "Distance at which detail prop
 ConVar cl_detailfade( "cl_detailfade", "400", 0, "Distance across which detail props fade in" );
 #if defined( USE_DETAIL_SHAPES ) 
 ConVar cl_detail_max_sway( "cl_detail_max_sway", "0", FCVAR_ARCHIVE, "Amplitude of the detail prop sway" );
-ConVar cl_detail_avoid_radius( "cl_detail_avoid_radius", "0", FCVAR_ARCHIVE, "radius around detail sprite to avoid players" );
-ConVar cl_detail_avoid_force( "cl_detail_avoid_force", "0", FCVAR_ARCHIVE, "force with which to avoid players ( in units, percentage of the width of the detail sprite )" );
-ConVar cl_detail_avoid_recover_speed( "cl_detail_avoid_recover_speed", "0", FCVAR_ARCHIVE, "how fast to recover position after avoiding players" );
+ConVar cl_detail_avoid_radius( "cl_detail_avoid_radius", "32", FCVAR_ARCHIVE, "radius around detail sprite to avoid players" );
+ConVar cl_detail_avoid_force( "cl_detail_avoid_force", "1", FCVAR_ARCHIVE, "force with which to avoid players ( in units, percentage of the width of the detail sprite )" );
+ConVar cl_detail_avoid_recover_speed( "cl_detail_avoid_recover_speed", "12", FCVAR_ARCHIVE, "how fast to recover position after avoiding players" );
 #endif
 
 // Per detail instance information
