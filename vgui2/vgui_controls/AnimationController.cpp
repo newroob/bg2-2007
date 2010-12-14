01cd03ce7b37f571e4fe9dfe5dc6b2e59dacd1db
bgmod <bgmod@d519a5cf-78fe-0310-83d0-8aec7336fec8>
2010-12-14 20:41:52 +0000

    .. -HP

--- vgui2/vgui_controls/AnimationController.cpp
+++ vgui2/vgui_controls/AnimationController.cpp
@@ -200,7 +200,7 @@ AnimationController::RelativeAlignment AnimationController::LookupAlignment( cha
 		}
 	}
 
-	return a_northwest;
+	return AnimationController::a_northwest;
 }
 
 //-----------------------------------------------------------------------------
